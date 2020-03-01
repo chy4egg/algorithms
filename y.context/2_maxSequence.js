8b6593a5a54d274bd8588a51069778f6e1615845
aleksandr <chy4egg@gmail.com>
2020-03-01 09:59:28 +0300

    fix

--- y.context/2_maxSequence.js
+++ y.context/2_maxSequence.js
@@ -1,34 +1,30 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin
 });
 
 let lines = [];
-let current = 0;
-let max = 0;
-
 rl.on("line", line => {
   lines.push(line);
 }).on("close", () => {
-
-  for (let i = 0; i < lines.length; i++) {
-
-    if (lines[i] === 1) {
+  let current = 0;
+  let max = 0;
+  for (let i = 1; i < lines.length; i++) {
+    if (lines[i] > 0) {
       current++;
-      // if its the last element
+      // last element check
       if (lines[i + 1] === undefined) {
         if (current > max) {
-          current = max;
+          max = current;
+          current = 0;
         }
       }
     } else {
       if (current > max) {
-        current = max;
+        max = current;
         current = 0;
       }
     }
   }
-
   process.stdout.write(max.toString());
 });
